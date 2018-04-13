9d55823ca5d74fad950e5581ce62681c8767f6a5
吉祥 夏 <1004864951@qq.com>
2018-04-13 20:50:01 +0800

    测试服务器能否开ssr

--- initcfg.sh
+++ initcfg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+#复制三个配置文件到需要的目录
 chmod +x *.sh
 chmod +x shadowsocks/*.sh
-cp -n apiconfig.py userapiconfig.py
+cp -n apiconfig.py userapiconfig.py   #-n 选项 不要覆盖已存在的文件
 cp -n config.json user-config.json
 cp -n mysql.json usermysql.json
 
